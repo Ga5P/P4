396072023714ffa44c1c2828c6d3643cea7e87dc
GasP <45510664+Ga5P@users.noreply.github.com>
2021-09-23 22:37:27 +0200

    Add files via upload
    - Fixed function which aims to modify a player

--- chessModel.py
+++ chessModel.py
@@ -1,40 +1,68 @@
 serialized_player = {}
 serialized_tournament = {}
 
+def get_num_player(val, dic):
+    for key, value in dic.items():
+        for k, v in value.items():
+            if val == v:
+                return key
+
+'''def save_seri(dic):
+    dic[get_num_player(p, dic)] = info
+    pass'''
+
 
 class Player:
     def __init__(self, name=None):
         self.name = name
 
-    def enter_infos(self):
+    def enter_infos(self, info):
+        valid_infos = ['age', 'last name', 'birth date', 'gender', 'rank', 'position in tournament']
+
+        num_player = input("Enter the player's name :\n")
+        if len(tournament.players) >= num_player:
+            pass
+        else:
+            return 'Please enter an number smaller than or equal to the number of challengers '
+
+        # For how many players ?
+        # For how many infos ?
+
+        serialized_player[get_num_player(p, serialized_player)] = info
         pass
 
-    def modify_infos(self):
+    def modify_info(self):
 
         p = input("Which Player?")
 
-        for player in tournament.players:
-            for key, value in vars(player).items():
+        for play in tournament.players:
+            for key, value in vars(play).items():
 
-                while p == value:
-                    print('You can change these informations; ', key, "\n")
-                    ask = input("Which info do you want to change?")
+                if p == value:
+                    while False: continue
+
+                    if True:
+                        print('You can change these informations; ', "\n", key)
+                        ask = input("Which info do you want to change?")
 
                     if key == ask:
+                        new = input(f'What is the new {ask} value?')
+                        serialized_player[get_num_player(p, serialized_player)] = {ask: new}
+                        setattr(play, f'{ask}', new)
 
-                        setattr(player, f'{ask}', input(f'What is the new {ask} value?'))
-                        print(f'The {ask} has been successfully changed ')
-                        return
-                else:
-                    print("Can't find this player")
-                    break
-            break
+                        return f'The {ask} has been successfully changed'
+
+                    else:
+                        return "can't find this info"
+        else:
+            return "Can't find this player"
+
 
 class Tournament:
     def __init__(self, name=None):
         self.name = name
 
-    def add_player(self):
+    def add_players(self):
         players = []
 
         num_players = int(input("Enter the number of tournament participants: "))
@@ -42,8 +70,8 @@ class Tournament:
         for i in range(num_players):
             player = Player()
             player.name = input("Enter the player's name :\n")
-
             players.append(player)
+            serialized_player[f'player{i + 1}'] = {'name': player.name}
 
         self.players = players
 
@@ -53,7 +81,7 @@ class Tournament:
 # ------------------------------------------------------------------- #
 
 tournament = Tournament()
-tournament.add_player()
+tournament.add_players()
 
 player = Player()
-player.modify_infos()
+player.modify_info()
