ab4a69d30fecd682bbb65e8c201f30b49f3889d5
GasP <45510664+Ga5P@users.noreply.github.com>
2021-10-21 19:44:55 +0200

    Add files via upload
    - Some usefull functions added
    - Some classes added
    - Add/Mofify informations is now possible

--- chessModel.py
+++ chessModel.py
@@ -1,27 +1,34 @@
+import random
+
 valid_tournament_info = ['name', 'place', 'date', 'players', 'description', 'score historic', 'round duration']
 valid_player_info = ['name', 'age', 'last name', 'birth date', 'gender', 'rank', 'position in tournament']
-
 
 serialized_player = {}
 serialized_tournament = {}
 
 def varname(instance):
-    try:
-        iter(instance)
-        if instance == tournament.players:
-            name = 'players'
-        #elif instance == tournament.score_historic:
-            #name = 'score historic'
+    if instance == tournament:
+        name = 'tournament'
         return name
-    except TypeError:
-        for name in globals():
-            if eval(name) == instance:
+
+    else:
+        try:
+            iter(instance)
+            if instance == tournament.players:
+                name = 'players'
                 return name
+
+        except TypeError:
+            for name in globals():
+                if eval(name) == instance:
+                    return name
+
 def get_num_player(val, dic):
     for key, value in dic.items():
         for k, v in value.items():
             if val == v:
                 return key
+
 def current_attributes(obj):
     try:
         iter(obj)
@@ -38,24 +45,32 @@ class Player:
     def __init__(self, name=None):
         self.name = name
 
-    def enter_infos(self, info):
+    #def enter_infos(self, info):
 
 
-        num_player = input("Enter the player's name :\n")
-        if len(tournament.players) >= num_player:
-            pass
-        else:
-            return 'Please enter an number smaller than or equal to the number of challengers '
+        #num_player = input("Enter the player's name :\n")
+        #if len(tournament.players) >= num_player:
+            #pass
+        #else:
+            #return 'Please enter an number smaller than or equal to the number of challengers '
 
         # For how many players ?
         # For how many infos ?
 
-        serialized_player[get_num_player(p, serialized_player)] = info
-        pass
+        #serialized_player[get_num_player(p, serialized_player)] = info
+        #pass
 
 class Tournament:
     def __init__(self, name=None):
         self.name = name
+
+    def new_tournament(self):
+
+        dates = []
+
+        tournament.name = str(input("Please enter the name of the tournament: \n"))
+
+        return tournament
 
     def add_players(self):
         players = []
@@ -70,7 +85,26 @@ class Tournament:
 
         self.players = players
 
-    def modif_infos(self, obj):
+    def new_round(self):
+        try:
+            if tournament.rounds:
+                rounds = tournament.rounds
+        except AttributeError:
+            rounds = []
+
+        round = Round()
+        round.name = f"round_{Round.counter + 1}"
+        rounds.append(round)
+
+        self.rounds = rounds
+        Round.counter += 1
+
+        pairs = round.generate_random_pairs()
+        return pairs
+
+
+
+    def modify_infos(self, obj):
 
         instance = varname(obj)
         modif = int(input(f'How many {instance} would you modify ?'))
@@ -116,16 +150,108 @@ class Tournament:
                         print(p)
                         if ask == key:
                             new = input(f'What is the new {ask} for {p}?')
-                            # serialized_player[get_num_player(p, serialized_player)] = {ask: new}
+                            serialized_tournament[tournament.name] = {ask: new}
                             setattr(obj, f'{ask}', new)
                             print(f'The {ask} has been successfully changed')
                             break
 
+    def add_infos(self, obj):
+        instance = varname(obj)
 
-# ------------------------------------------------------------------- #
-# ------------------- WILL BE IN THE VIEW PART ---------------------- #
-# ------------------------------------------------------------------- #
+        if instance == 'players':
+
+            for play in tournament.players:
+
+                valid = [k for k in valid_player_info if k and k.replace(' ', '_') not in current_attributes(tournament.players)]
+
+            print('The list of informations you can add', valid)
+            ask = input("Which info do you want to add ?")
+
+            for play in tournament.players:
+
+                if not valid:
+                    print(f"You can't add another information for {instance}")
+                    return
+                else:
+                    for i in valid:
+                        if i == ask:
+                            new = input(f'What is the {ask} of {play.name} ?')
+                            asko = ask.replace(' ', '_')
+                            setattr(play, f'{asko}', new)
+
+        elif instance == 'tournament':
+            valid = [i for i in valid_tournament_info if i not in current_attributes(obj)]
+            if not valid:
+                print(f"You can't add another information for this {instance}")
+                return
+            else:
+                print('The list of informations you can add', valid)
+                ask = input("Which info do you want to add ?")
+                for i in valid:
+                    if i == ask:
+                        new = input(f'What is the {ask} of the {instance} ?')
+                        asko = ask.replace(' ', '_')
+                        setattr(obj, f'{asko}', new)
+
+class Round:
+
+    counter = 0
+
+    def __init__(self):
+        pass
+
+# Chaque paire fait un match
+
+    def new_match(self, pairs):
+        matchs = []
+        iter = 0
+
+        for pair in pairs:
+
+            match = Match()
+            match.paired_players = pair
+            match.name = f"match_{iter + 1}"
+            matchs.append(match)
+
+            iter += 1
+
+        self.matchs = matchs
+
+    def generate_random_pairs(self):
+
+        p = random.sample(tournament.players, len(tournament.players))
+        pairs = [tuple(p[x:x + 2]) for x in range(0, len(p), 2)]
+
+        print('--------------------------------------------------------')
+        print(pairs[0][0].name, " will fight against ", pairs[0][1].name)
+        print(pairs[1][0].name, " will fight against ", pairs[1][1].name)
+
+        return pairs
+
+class Match:
+
+    def __init__(self):
+        pass
+
+
+
+# ------------------------------------------------------------------ #
+# ------------------- WILL BE IN THE VIEW PART --------------------- #
+# ------------------ FROM NOW, IT'S THE TEST PART ------------------ #
+# ------------------------------------------------------------------ #
+
 
 tournament = Tournament()
+tournament.new_tournament()
 tournament.add_players()
-tournament.modif_infos(tournament.players)
+
+#tournament.modify_infos(tournament.players)
+#tournament.modify_infos(tournament)
+
+#tournament.add_infos(tournament)
+#tournament.add_infos(tournament.players)
+
+#tournament.new_round()
+
+#round = Round()
+#round.new_match(tournament.new_round())
